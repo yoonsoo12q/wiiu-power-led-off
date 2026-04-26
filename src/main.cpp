bb1c35562a6bcd4f71bd9109ede438e605cdba7d
yoonsoo12q <yoonsoo12q@gmail.com>
2026-04-26 11:01:02 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,49 @@
 #include "utils/logger.h"
+#include <coreinit/dynload.h>
 #include <coreinit/energysaver.h>
 #include <coreinit/thread.h>
 #include <wups.h>
 #include <wups/config_api.h>
 #include <wups/config/WUPSConfigItemBoolean.h>
 
-/**
-    Mandatory plugin information.
-**/
 WUPS_PLUGIN_NAME("Power LED Off");
 WUPS_PLUGIN_DESCRIPTION("A WUPS plugin to turn off the Wii U console's front power LED.");
 WUPS_PLUGIN_VERSION("v1.0");
 WUPS_PLUGIN_AUTHOR("Manus");
 WUPS_PLUGIN_LICENSE("GPL");
-
 WUPS_USE_WUT_DEVOPTAB();
 WUPS_USE_STORAGE("power_led_off");
 
 #define ENABLE_LED_OFF_CONFIG_ID "enable_led_off"
 bool sEnableLedOff = true;
 
-/**
- * SMC I2C Register commands for LEDs
- * Device ID: 0x50 on I2C bus 3
- * 0x11: SMC_CMD_PWRLED_ON
- * 0x12: SMC_CMD_PWRLED_OFF
- * 0x13: SMC_CMD_PWRLED_BLINK
- */
+// OSWriteI2C 함수 포인터
+typedef int (*OSWriteI2C_t)(int bus, int device, const void *data, int size);
+static OSWriteI2C_t sOSWriteI2C = nullptr;
 
-// I2C Write function signature (internal coreinit function)
-extern "C" int OSWriteI2C(int bus, int device, const void *data, int size);
+bool initI2C() {
+    OSDynLoad_Module coreinit;
+    if (OSDynLoad_Acquire("coreinit.rpl", &coreinit) != OS_DYNLOAD_OK) {
+        return false;
+    }
+    if (OSDynLoad_FindExport(coreinit, OS_DYNLOAD_EXPORT_FUNC,
+                              "OSWriteI2C", (void **)&sOSWriteI2C) != OS_DYNLOAD_OK) {
+        sOSWriteI2C = nullptr;
+        OSDynLoad_Release(coreinit);
+        return false;
+    }
+    OSDynLoad_Release(coreinit);
+    return true;
+}
 
 void setPowerLedState(bool off) {
+    if (!sOSWriteI2C) return;
     if (off) {
-        // SMC_CMD_PWRLED_OFF (0x12)
-        uint8_t data[1] = { 0x12 };
-        OSWriteI2C(3, 0x50, data, 1);
+        uint8_t data[1] = { 0x12 }; // SMC_CMD_PWRLED_OFF
+        sOSWriteI2C(3, 0x50, data, 1);
     } else {
-        // SMC_CMD_PWRLED_ON (0x11)
-        uint8_t data[1] = { 0x11 };
-        OSWriteI2C(3, 0x50, data, 1);
+        uint8_t data[1] = { 0x11 }; // SMC_CMD_PWRLED_ON
+        sOSWriteI2C(3, 0x50, data, 1);
     }
 }
 
@@ -69,23 +73,22 @@ void ConfigMenuClosedCallback() {
 
 INITIALIZE_PLUGIN() {
     initLogging();
-    
+
+    initI2C(); // OSWriteI2C 주소 획득
+
     WUPSConfigAPIOptionsV1 configOptions = {.name = "Power LED Off"};
     WUPSConfigAPI_Init(configOptions, ConfigMenuOpenedCallback, ConfigMenuClosedCallback);
-
     WUPSStorageAPI::GetOrStoreDefault(ENABLE_LED_OFF_CONFIG_ID, sEnableLedOff, true);
     WUPSStorageAPI::SaveStorage();
 
-    // Turn off LED if enabled
     if (sEnableLedOff) {
         setPowerLedState(true);
     }
-    
+
     deinitLogging();
 }
 
 DEINITIALIZE_PLUGIN() {
-    // Restore LED state when plugin is unloaded
     setPowerLedState(false);
 }
 
